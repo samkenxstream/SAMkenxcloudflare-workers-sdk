12d685cf42b64e3a7d03715425f11fbc2d2307f5
Jacob M-G Evans <jacobmgevans@gmail.com>
2022-03-30 15:15:05 -0500

    tests: snapshots updated with emojis for deprecation messaging.

--- packages/wrangler/src/__tests__/publish.test.ts
+++ packages/wrangler/src/__tests__/publish.test.ts
@@ -442,7 +442,7 @@ describe("publish", () => {
       expect(std.err).toMatchInlineSnapshot(`""`);
       expect(std.warn).toMatchInlineSnapshot(`
         "Processing wrangler.toml configuration:
-          - DEPRECATION: \\"build.upload.main\\":
+          - 🦺 DEPRECATION: \\"build.upload.main\\":
             Delete the \`build.upload.main\` and \`build.upload.dir\` fields.
             Then add the top level \`main\` field to your configuration file:
             \`\`\`
@@ -474,13 +474,13 @@ describe("publish", () => {
       expect(std.err).toMatchInlineSnapshot(`""`);
       expect(std.warn).toMatchInlineSnapshot(`
         "Processing ../wrangler.toml configuration:
-          - DEPRECATION: \\"build.upload.main\\":
+          - 🦺 DEPRECATION: \\"build.upload.main\\":
             Delete the \`build.upload.main\` and \`build.upload.dir\` fields.
             Then add the top level \`main\` field to your configuration file:
             \`\`\`
             main = \\"foo/index.js\\"
             \`\`\`
-          - DEPRECATION: \\"build.upload.dir\\":
+          - 🦺 DEPRECATION: \\"build.upload.dir\\":
             Use the top level \\"main\\" field or a command-line argument to specify the entry-point for the Worker."
       `);
     });
@@ -654,7 +654,7 @@ export default{
       await expect(runWrangler("publish ./index.js")).rejects
         .toThrowErrorMatchingInlineSnapshot(`
               "Processing wrangler.toml configuration:
-                - DEPRECATION: \\"site.entry-point\\":
+                - 🚨 DEPRECATION: \\"site.entry-point\\":
                   The \`site.entry-point\` config field is no longer used.
                   The entry-point should be specified via the command line or the \`main\` config field."
             `);
@@ -662,7 +662,7 @@ export default{
       expect(std.out).toMatchInlineSnapshot(`""`);
       expect(std.err).toMatchInlineSnapshot(`
         "Processing wrangler.toml configuration:
-          - DEPRECATION: \\"site.entry-point\\":
+          - 🚨 DEPRECATION: \\"site.entry-point\\":
             The \`site.entry-point\` config field is no longer used.
             The entry-point should be specified via the command line or the \`main\` config field.
 
